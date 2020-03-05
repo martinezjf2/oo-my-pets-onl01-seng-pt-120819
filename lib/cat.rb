ccfffb8e393678810b893efb7b7e3625156764b4
Jeffrey Martinez <martinezjf2@gmail.com>
2020-03-05 05:03:21 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
+  attr_accessor :owner
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+   
+    
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
   # code goes here
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = 'human'
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
   # code goes here
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+    
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  
 end
