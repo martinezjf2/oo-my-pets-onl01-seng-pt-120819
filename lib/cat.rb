3037a125b3601e0471a290541c5d9a6c0872ba17
github username <github email address>
2020-09-05 15:39:12 -0400

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,26 @@
 class Cat
   # code goes here
+  attr_reader :name
+  attr_accessor :mood, :owner
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = 'nervous'
+    @@all << self
+  end
+
+  def mood
+    @mood
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
 class Owner
   # code goes here
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def species
+     return "human"
+  end
+
+  def say_species
+    return 'I am a human.'
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+   self.all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+
 end
